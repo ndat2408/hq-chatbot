438543b456846483f589043520411d40909eb051
dodoproptit99 <dodo.proptit.99@gmail.com>
2020-06-12 11:40:52 +0700

    updated

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$DBHOST = getenv('DBHOST'); // Set your database host here
+$DBUSER = getenv('DBUSER'); // Set your database user here
+$DBNAME = getenv('DBNAME'); // Set your database name here
+$DBPW =  getenv('DBPASS'); // Set your database password here
+$BOT_ID = getenv('BOT_ID');
+$TOKEN = getenv('BOT_TOKEN');
+
+
+$BLOCK_NAME = "hq_chat";
+$JSON = "hq_text";
+$BLOCK_IMAGE = "hq_image";
+$BLOCK_VOID = "hq_void";
+$BLOCK_VIDEO = "hq_video";
+$BLOCK_FILE = "hq_file";
+
+?>
